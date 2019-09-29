d44747a93952c24ef42f62ff07493bd936ea741a
AlexDest13 <perseus.ua@gmail.com>
2019-09-29 18:17:29 +0300

    HW00

--- HW00/src/HW00.java
+++ HW00/src/HW00.java
@@ -2,7 +2,13 @@ public class HW00 {
     public static void main(String[] args) {
         var a = 1;
         var b = 2;
+        var c = 4.3;
         var d = 10;
+        var e = 6.6;
         System.out.println((float)(a+b)/(d-b)*2);
+        System.out.println((a+2)*(2-12)/(a*(b-c)));
+        System.out.println((b+11)*(c-a));
+        System.out.println(a*(c+b+d)-(e/(1+a)));
+        System.out.println((float)(a+b)*(a-b)/12);
     }
 }
