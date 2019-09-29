cad1aefbf5fbd191fce10937a6f86437f62e14c7
AlexDest13 <perseus.ua@gmail.com>
2019-09-29 13:04:08 +0300

    тест

---
+++ HW00/src/HW00.java
@@ -0,0 +1,8 @@
+public class HW00 {
+    public static void main(String[] args) {
+        var a = 1;
+        var b = 2;
+        var d = 10;
+        System.out.println((float)(a+b)/(d-b)*2);
+    }
+}
